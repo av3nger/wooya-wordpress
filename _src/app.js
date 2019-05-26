99bd6892b2aa49e657b61697cc67009342374190
Anton Vanyukov <anton@incsub.com>
2019-05-26 22:11:58 +0300

    Upgrade dependencies

--- _src/app.js
+++ _src/app.js
@@ -1,10 +1,19 @@
+/**
+ * External dependencies
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
-import './app.scss';
+/**
+ * WordPress dependencies
+ */
+const { __ } = wooyaI18n;
 
-const {__} = wooyaI18n;
+/**
+ * Internal dependencies
+ */
+import './app.scss';
 
 import FetchWP from './utils/fetchWP';
 
@@ -18,229 +27,228 @@ import YmlListControl from './components/yml-list-control';
  * Wooya React component
  */
 class Wooya extends React.Component {
-  /**
-   * Wooya constructor
-   *
-   * @param {object} props
-   */
-  constructor(props) {
-    super(props);
+	/**
+	 * Wooya constructor
+	 *
+	 * @param {Object} props
+	 */
+	constructor( props ) {
+		super( props );
 
-    this.state = {
-      loading: true,
-      options: [], // Plugin options.
-      fields: [], // List of available fields.
-      unusedItems: [], // Not used fields (available to add via modal).
-      error: {
-        show: false,
-        message: '',
-        link: '',
-      },
-      showProgressModal: false,
-    };
+		this.state = {
+			loading: true,
+			options: [], // Plugin options.
+			fields: [], // List of available fields.
+			unusedItems: [], // Not used fields (available to add via modal).
+			error: {
+				show: false,
+				message: '',
+				link: '',
+			},
+			showProgressModal: false,
+		};
 
-    /**
-     * @type {FetchWP}
-     * @param {string} this.props.wpObject.api_url
-     * @param {string} this.props.wpObject.api_nonce
-     */
-    this.fetchWP = new FetchWP({
-      restURL: this.props.wpObject.api_url,
-      restNonce: this.props.wpObject.api_nonce,
-    });
+		/**
+		 * @type {FetchWP}
+		 * @param {string} this.props.wpObject.api_url
+		 * @param {string} this.props.wpObject.api_nonce
+		 */
+		this.fetchWP = new FetchWP( {
+			restURL: this.props.wpObject.api_url,
+			restNonce: this.props.wpObject.api_nonce,
+		} );
 
-    const notice = document.getElementById('rate-notice');
-    if ( notice ) {
-      notice.addEventListener('click', () => {
-        const xhr = new XMLHttpRequest();
-        xhr.open(
-          'POST',
-          ajaxurl+'?action=dismiss_rate_notice&_ajax_nonce='+this.props.wpObject.api_nonce
-        );
-        xhr.send();
-      });
-    }
-  }
+		const notice = document.getElementById( 'rate-notice' );
+		if ( notice ) {
+			notice.addEventListener( 'click', () => {
+				const xhr = new XMLHttpRequest();
+				xhr.open(
+					'POST',
+					ajaxurl + '?action=dismiss_rate_notice&_ajax_nonce=' + this.props.wpObject.api_nonce
+				);
+				xhr.send();
+			} );
+		}
+	}
 
-  /**
-   * Init component states
-   */
-  componentDidMount() {
-    this.fetchWP.get('settings').then(
-        (json) => this.getElements(json),
-        (err) => this.showError(0, err.message)
-    );
-  }
+	/**
+	 * Init component states
+	 */
+	componentDidMount() {
+		this.fetchWP.get( 'settings' ).then(
+			( json ) => this.getElements( json ),
+			( err ) => this.showError( 0, err.message )
+		);
+	}
 
-  /**
-   * Get all the elements and sort out the ones that are in use to a
-   * separate variable.
-   *
-   * @param {json} options
-   */
-  getElements(options) {
-    this.fetchWP.get('elements').then(
-        (json) => {
-          const unusedItems = {
-            shop: [],
-            offer: [],
-          };
+	/**
+	 * Get all the elements and sort out the ones that are in use to a
+	 * separate variable.
+	 *
+	 * @param {json} options
+	 */
+	getElements( options ) {
+		this.fetchWP.get( 'elements' ).then(
+			( json ) => {
+				const unusedItems = {
+					shop: [],
+					offer: [],
+				};
 
-          Object.entries(json).forEach((type) => {
-            Object.keys(type[1]).filter((element) => {
-              if ( ! ( type[0] in unusedItems ) ) {
-                return true;
-              }
+				Object.entries( json ).forEach( ( type ) => {
+					Object.keys( type[ 1 ] ).filter( ( element ) => {
+						if ( ! ( type[ 0 ] in unusedItems ) ) {
+							return true;
+						}
 
-              const unused = 'undefined' === typeof options[type[0]];
-              if ( unused || ! ( element in options[type[0]] ) ) {
-                unusedItems[type[0]].push(element);
-                return false;
-              }
+						const unused = 'undefined' === typeof options[ type[ 0 ] ];
+						if ( unused || ! ( element in options[ type[ 0 ] ] ) ) {
+							unusedItems[ type[ 0 ] ].push( element );
+							return false;
+						}
 
-              return true;
-            });
-          });
+						return true;
+					} );
+				} );
 
-          this.setState({
-            loading: false,
-            options: options,
-            fields: json,
-            unusedItems: unusedItems,
-          });
-        },
-        (err) => this.showError(0, err.message)
-    );
-  }
+				this.setState( {
+					loading: false,
+					options,
+					fields: json,
+					unusedItems,
+				} );
+			},
+			( err ) => this.showError( 0, err.message )
+		);
+	}
 
-  /**
-   * Display error notice.
-   *
-   * @param {int} code        Error message code.
-   * @param {string} message  Error message text.
-   * @param {string} link     Link for more info button in error message.
-   */
-  showError(code, message = '', link = '') {
-    if ( 'undefined' === typeof code ) {
-      return;
-    }
+	/**
+	 * Display error notice.
+	 *
+	 * @param {number} code     Error message code.
+	 * @param {string} message  Error message text.
+	 * @param {string} link     Link for more info button in error message.
+	 */
+	showError( code, message = '', link = '' ) {
+		if ( 'undefined' === typeof code ) {
+			return;
+		}
 
-    // Try to look for error message by code.
-    if ( '' === message ) {
-      message = ajax_strings.errors['error_' + code];
-    }
+		// Try to look for error message by code.
+		if ( '' === message ) {
+			message = ajax_strings.errors[ 'error_' + code ];
+		}
 
-    if ( '' === link ) {
-      link = ajax_strings.errors['link_' + code];
-    }
+		if ( '' === link ) {
+			link = ajax_strings.errors[ 'link_' + code ];
+		}
 
-    this.setState({
-      loading: false,
-      error: {
-        show: true,
-        message: message,
-        link: link,
-      },
-      showProgressModal: false,
-    });
-  }
+		this.setState( {
+			loading: false,
+			error: {
+				show: true,
+				message,
+				link,
+			},
+			showProgressModal: false,
+		} );
+	}
 
-  /**
-   * Hide error message.
-   */
-  hideError() {
-    this.setState({
-      error: {
-        show: false,
-        message: '',
-        link: '',
-      },
-    });
-  }
+	/**
+	 * Hide error message.
+	 */
+	hideError() {
+		this.setState( {
+			error: {
+				show: false,
+				message: '',
+				link: '',
+			},
+		} );
+	}
 
-  /**
-   * Handle update in the UI settings form.
-   *
-   * @param {array} options
-   * @param {array} unusedItems
-   */
-  handleUpdate(options, unusedItems) {
-    this.setState({
-      loading: false, // TODO: is this needed?
-      options: options,
-      unusedItems: unusedItems,
-    });
-  }
+	/**
+	 * Handle update in the UI settings form.
+	 *
+	 * @param {Array} options
+	 * @param {Array} unusedItems
+	 */
+	handleUpdate( options, unusedItems ) {
+		this.setState( {
+			loading: false, // TODO: is this needed?
+			options,
+			unusedItems,
+		} );
+	}
 
-  /**
-   * Render component
-   *
-   * @return {*}
-   */
-  render() {
-    return (
-      <div className="me-main-content">
-        {this.state.error.show &&
-        <Notice type='error'
-          message={this.state.error.message}
-          link={this.state.error.link}
-          onHide={this.hideError}
-        />}
+	/**
+	 * Render component
+	 *
+	 * @return {*}  App.
+	 */
+	render() {
+		return (
+			<div className="me-main-content">
+				{ this.state.error.show &&
+				<Notice type="error"
+					message={ this.state.error.message }
+					link={ this.state.error.link }
+					onHide={ this.hideError }
+				/> }
 
-        {this.state.options &&
-        <Button
-          buttonText={__( 'Generate YML', 'wooya' )}
-          className='wooya-btn wooya-btn-red wooya-btn-center'
-          onClick={() => this.setState({
-            showProgressModal: ! this.state.showProgressModal,
-          })}
-        /> }
+				{ this.state.options &&
+				<Button
+					buttonText={ __( 'Generate YML', 'wooya' ) }
+					className="wooya-btn wooya-btn-red wooya-btn-center"
+					onClick={ () => this.setState( {
+						showProgressModal: ! this.state.showProgressModal,
+					} ) }
+				/> }
 
-        <Files
-          fetchWP={this.fetchWP}
-          onError={(error) => this.showError(0, error)}
-        />
+				<Files
+					fetchWP={ this.fetchWP }
+					onError={ ( error ) => this.showError( 0, error ) }
+				/>
 
-        <YmlListControl
-          options={this.state.options}
-          fields={this.state.fields}
-          unusedItems={this.state.unusedItems}
-          onError={(error) => this.showError(0, error)}
-          handleUpdate={(options, unusedItems) => {
-            this.handleUpdate(options, unusedItems);
-          }}
-          fetchWP={this.fetchWP}
-        />
+				<YmlListControl
+					options={ this.state.options }
+					fields={ this.state.fields }
+					unusedItems={ this.state.unusedItems }
+					onError={ ( error ) => this.showError( 0, error ) }
+					handleUpdate={ ( options, unusedItems ) => {
+						this.handleUpdate( options, unusedItems );
+					} }
+					fetchWP={ this.fetchWP }
+				/>
 
-        {this.state.showProgressModal &&
-        <ProgressModal
-          onFinish={() => {
-            Files.updateFileList();
-            this.setState({showProgressModal: false});
-          }}
-          onError={(errorCode) => {
-            this.setState({showAddDiv: false});
-            this.showError(errorCode);
-          }}
-          fetchWP={this.fetchWP}
-        />
-        }
-      </div>
-    );
-  }
+				{ this.state.showProgressModal &&
+				<ProgressModal
+					onFinish={ () => {
+						Files.updateFileList();
+						this.setState( { showProgressModal: false } );
+					} }
+					onError={ ( errorCode ) => {
+						this.setState( { showAddDiv: false } );
+						this.showError( errorCode );
+					} }
+					fetchWP={ this.fetchWP }
+				/> }
+			</div>
+		);
+	}
 }
 
 Wooya.propTypes = {
-  wpObject: PropTypes.object,
+	wpObject: PropTypes.object,
 };
 
-document.addEventListener('DOMContentLoaded', function() {
-  const container = document.getElementById('wooya_components');
-  if ( container ) {
-    ReactDOM.render(
-        /** @var {object} window.ajax_strings */
-        <Wooya wpObject={window.ajax_strings}/>,
-        container
-    );
-  }
-});
+document.addEventListener( 'DOMContentLoaded', function() {
+	const wooyaDiv = document.getElementById( 'wooya_components' );
+	if ( wooyaDiv ) {
+		ReactDOM.render(
+			/** @var {object} window.ajax_strings */
+			<Wooya wpObject={ window.ajax_strings } />,
+			wooyaDiv
+		);
+	}
+} );
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const autoprefixer = require( 'autoprefixer' );
 
 // Plugins
 const ExtractTextPlugin = require( 'mini-css-extract-plugin' );
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CleanWebpackPlugin = require( 'clean-webpack-plugin' );
 
 module.exports = {
 	mode: 'production',
@@ -16,19 +16,19 @@ module.exports = {
 
 	output: {
 		filename: '[name].min.js',
-		path: path.resolve( __dirname, 'admin/js' )
+		path: path.resolve( __dirname, 'admin/js' ),
 	},
 
 	module: {
-		rules: [{
+		rules: [ {
 			test: /\.(js|jsx)$/,
 			exclude: /node_modules/,
 			use: {
 				loader: 'babel-loader',
 				options: {
-					presets: [ '@babel/preset-env', '@babel/preset-react' ]
-				}
-			}
+					presets: [ '@babel/preset-env', '@babel/preset-react' ],
+				},
+			},
 		}, {
 			test: /\.scss$/,
 			exclude: /node_modules/,
@@ -36,69 +36,66 @@ module.exports = {
 				{
 					loader: 'css-loader',
 					options: {
-						sourceMap: true
-					}
+						sourceMap: true,
+					},
 				}, {
 					loader: 'sass-loader',
 					options: {
 						sourceMap: true,
 						plugins: [
-							autoprefixer({
-								browsers:['ie >= 8', 'last 3 version']
-							})
+							autoprefixer( {
+								browsers: [ 'ie >= 8', 'last 3 version' ],
+							} ),
 						],
-					}
-				}
-			]
+					},
+				},
+			],
 		}, {
 			test: /\.(png|jpg|gif)$/,
 			use: {
 				loader: 'file-loader',
 				options: {
 					name: '[name].[ext]',
-					outputPath: '../images/'
-				}
-			}
+					outputPath: '../images/',
+				},
+			},
 		}, {
 			test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
 			use: {
 				loader: 'file-loader',
 				options: {
 					name: '[name].[ext]',
-					outputPath: '../fonts/'
-				}
-			}
-		} ]
+					outputPath: '../fonts/',
+				},
+			},
+		} ],
 	},
 
 	// This will allow us to import files without writing these extension
 	// eg: import 'app', instead of import 'app.jsx'
 	resolve: {
-		extensions: [".js", ".jsx", ".json"],
+		extensions: [ '.js', '.jsx', '.json' ],
 	},
 
 	plugins: [
-		new CleanWebpackPlugin([
-			'admin/js',
-			'admin/css'
-		]),
-		new ExtractTextPlugin({
-			filename: '../css/[name].min.css'
-		}),
-		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify( 'production' )
-		})
+		new CleanWebpackPlugin(),
+		new ExtractTextPlugin( {
+			filename: '../css/[name].min.css',
+		} ),
+		new webpack.DefinePlugin( {
+			'process.env.NODE_ENV': JSON.stringify( 'production' ),
+		} ),
 	],
 
 	devtool: 'source-map', // Generates source Maps for these files
 
 	stats: {
 		colors: true,
-		entrypoints: true
+		entrypoints: true,
 	},
 
 	watchOptions: {
 		ignored: /node_modules/,
-		poll: 1000
-	}
+		poll: 1000,
+	},
 };
