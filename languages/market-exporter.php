78ce669c6cccb91b15208e3667eb9163e67ffe76
Anton Vanyukov <anton@incsub.com>
2019-06-15 11:53:53 +0300

    Update language files

---
+++ languages/market-exporter.php
@@ -0,0 +1,67 @@
+<?php
+/* THIS IS A GENERATED FILE. DO NOT EDIT DIRECTLY. */
+$generated_i18n_strings = array(
+	// Reference: _src/app.js:201
+	__( 'Generate YML', 'market-exporter' ),
+
+	// Reference: _src/components/add-setting-modal/index.jsx:125
+	// Reference: _src/components/yml-list-control/index.jsx:224
+	__( 'Add new setting', 'market-exporter' ),
+
+	// Reference: _src/components/add-setting-modal/index.jsx:128
+	// Reference: _src/components/yml-list-control/index.jsx:243
+	__( 'Shop', 'market-exporter' ),
+
+	// Reference: _src/components/add-setting-modal/index.jsx:137
+	// Reference: _src/components/yml-list-control/index.jsx:250
+	__( 'Offer', 'market-exporter' ),
+
+	// Reference: _src/components/add-setting-modal/index.jsx:152
+	__( 'Select setting', 'market-exporter' ),
+
+	// Reference: _src/components/add-setting-modal/index.jsx:157
+	__( 'No available items to select', 'market-exporter' ),
+
+	// Reference: _src/components/add-setting-modal/index.jsx:163
+	__( 'Add items', 'market-exporter' ),
+
+	// Reference: _src/components/files/index.jsx:164
+	__( 'Available YML files', 'market-exporter' ),
+
+	// Reference: _src/components/files/index.jsx:170
+	__( 'Remove files', 'market-exporter' ),
+
+	// Reference: _src/components/files/index.jsx:181
+	__( 'You have not yet generated any files', 'market-exporter' ),
+
+	// Reference: _src/components/progress-modal/index.jsx:101
+	__( 'Generating file', 'market-exporter' ),
+
+	// Reference: _src/components/progress-modal/index.jsx:87
+	__( 'Initializing generation engine...', 'market-exporter' ),
+
+	// Reference: _src/components/progress-modal/index.jsx:89
+	__( 'Finalizing...', 'market-exporter' ),
+
+	// Reference: _src/components/progress-modal/index.jsx:92
+	__( 'Generating file: step %d of %d', 'market-exporter' ),
+
+	// Reference: _src/components/yml-list-control/index.jsx:219
+	__( 'Settings', 'market-exporter' ),
+
+	// Reference: _src/components/yml-list-control/index.jsx:231
+	__( 'Remove settings', 'market-exporter' ),
+
+	// Reference: _src/components/yml-list-control/index.jsx:258
+	__( 'Delivery options', 'market-exporter' ),
+
+	// Reference: _src/components/yml-list-control/index.jsx:265
+	__( 'Misc', 'market-exporter' ),
+
+	// Reference: _src/components/yml-list-item/index.jsx:164
+	__( 'Disabled', 'market-exporter' ),
+
+	// Reference: _src/components/yml-list-item/index.jsx:173
+	__( 'Active', 'market-exporter' )
+);
+/* THIS IS THE END OF THE GENERATED FILE */
