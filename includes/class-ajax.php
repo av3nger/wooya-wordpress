a4a53c21d4372ddf5e4a082c820c56131ad50c63
Anton Vanyukov <anton@incsub.com>
2019-07-22 20:45:44 +0300

    Add AJAX getter methods for settigns and elements

--- includes/class-ajax.php
+++ includes/class-ajax.php
@@ -20,7 +20,78 @@ class Ajax {
 	 * Ajax constructor.
 	 */
 	public function __construct() {
+
+		add_action( 'wp_ajax_me_settings', array( $this, 'get_settings' ) );
+		add_action( 'wp_ajax_me_elements', array( $this, 'get_combined_elements' ) );
 		add_action( 'wp_ajax_me_files', array( $this, 'get_files' ) );
+
+	}
+
+	/**
+	 * Get settings.
+	 *
+	 * @return array
+	 */
+	public function get_settings() {
+
+		$current_settings = get_option( 'wooya_settings' );
+
+		$elements = Elements::get_elements();
+
+		if ( ! isset( $current_settings['delivery'] ) ) {
+			foreach ( $elements['delivery'] as $element => $data ) {
+				$current_settings['delivery'][ $element ] = $data['default'];
+			}
+		}
+
+		if ( ! isset( $current_settings['misc'] ) ) {
+			foreach ( $elements['misc'] as $element => $data ) {
+				$current_settings['misc'][ $element ] = $data['default'];
+			}
+		}
+
+		if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+			wp_send_json_success( $current_settings );
+		}
+
+		return $current_settings;
+
+	}
+
+	/**
+	 * Get array of all elements.
+	 *
+	 * @since 2.0.0
+	 */
+	public function get_combined_elements() {
+
+		$elements = Elements::get_elements();
+		wp_send_json_success( $elements );
+
+	}
+
+	/**
+	 * Get YML elements array.
+	 */
+	public function get_elements() {
+
+		$method = "get_{$request['type']}_elements";
+
+		if ( ! method_exists( __NAMESPACE__ . '\\Elements', $method ) ) {
+			wp_send_json_error(
+				printf(
+					/* translators: %s: method name */
+					esc_html__( 'Method %s not found.', 'market-exporter' ),
+					esc_html( $method )
+				),
+				'method-not-found'
+			);
+		}
+
+		$elements = call_user_func( [ __NAMESPACE__ . '\\Elements', $method ] );
+
+		wp_send_json_success( $elements );
+
 	}
 
 	/**
