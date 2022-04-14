9762cbe2d84c58c88629196dee3dd251c30442ca
Anton Vanyukov <anton@incsub.com>
2022-04-14 10:26:13 +1000

    Adjust webpack build scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es6': true,
-  },
-  'extends': 'plugin:@wordpress/eslint-plugin/recommended',
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly',
-  },
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
-    },
-    'ecmaVersion': 2018,
-    'sourceType': 'module',
-  },
-  'rules': {
-  },
+	env: {
+		browser: true,
+		es6: true,
+	},
+	extends: [ 'plugin:@wordpress/eslint-plugin/recommended-with-formatting' ],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+	},
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	rules: {
+	},
 };
