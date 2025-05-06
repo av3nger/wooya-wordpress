08aa4feba5b9729767e071df50e072109ecaab7f
Anton Vanyukov <a.vanyukov@vcore.ru>
2025-05-06 16:46:00 +1000

    Add new filter

--- includes/class-attributes.php
+++ includes/class-attributes.php
@@ -120,7 +120,7 @@ class Attributes {
 				$old_price = $this->settings['misc']['old_price'];
 			}
 
-			$yml .= $this->add_child( $old_price, $offer->get_regular_price() );
+			$yml .= $this->add_child( $old_price, apply_filters( 'me_product_price', $offer->get_regular_price(), $offer->get_id() ) );
 		} else {
 			$yml = $this->add_child( 'price', apply_filters( 'me_product_price', $offer->get_regular_price(), $offer->get_id() ) );
 		}
